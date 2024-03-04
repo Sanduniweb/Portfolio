a6f5a786f1550c1787faffd74173ef14b52f9bfd
Sanduni Lakshika <136353555+Sanduniweb@users.noreply.github.com>
2024-03-04 14:07:47 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Portfolio
+# My Portfolio - Personal Portfolio Website
+
+Welcome to PortFlix, my personal portfolio website where you can showcase my work and skills as a graphic designer.
+
+## Sections
+
+- [Home](#home)
+- [About](#about)
+- [Skills](#skills)
+- [Services](#services)
+- [Gallery](#gallery)
+- [Contact](#contact)
+
+ ## Screen record
+- Flie name Myportfolio.mp4
+
+## Features
+
+- Responsive design
+- Easy navigation
+- Showcase your work and skills
+- Containing the my resume or CV for download
+- image galleries to enhance the user experience
+
+
+
---
+++ script.js
@@ -0,0 +1,64 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
+      var nameInput = document.getElementById('name');
+      var emailInput = document.getElementById('email');
+      var messageInput = document.getElementById('message');
+      
+      // Perform validation checks on the input fields
+      if (nameInput.value.trim() === '') {
+          alert('Please enter your name.');
+          return;
+      }
+      
+      if (emailInput.value.trim() === '') {
+          alert('Please enter your email.');
+          return;
+      }
+      
+      if (messageInput.value.trim() === '') {
+          alert('Please enter a message.');
+          return;
+      }
+      
+      // Submit the form
+      contactForm.submit();
+  };
+  var swiper = new Swiper(`[unique-script-id="w-w-dm-id"] .mySwiper`, {
+    slidesPerView: 1.5,
+    centeredSlides: true,
+    spaceBetween: 5,
+    pagination: {
+      el: `[unique-script-id="w-w-dm-id"] .swiper-pagination`,
+      clickable: true,
+    },
+    loop: true,
+    navigation: {
+      nextEl: `[unique-script-id="w-w-dm-id"] .swiper-button-nexts`,
+      prevEl: `[unique-script-id="w-w-dm-id"] .swiper-button-prevs`,
+    },
+  });
